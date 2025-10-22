30a4687d81a7439cb19a2aa8f7dfbcd10f9888ad
Asempere123123 <asempere158@gmail.com>
2025-10-22 19:34:45 +0200

    Added websocket server

---
+++ websocket/playback_log.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# check if a file argument is given
+if [ -z "$1" ]; then
+    echo "Usage: $0 <can_log_file>"
+    exit 1
+fi
+
+LOGFILE="$1"
+
+# check if file exists
+if [ ! -f "$LOGFILE" ]; then
+    echo "Error: File '$LOGFILE' not found!"
+    exit 1
+fi
+
+# load vcan module if not already loaded
+if ! lsmod | grep -q '^vcan'; then
+    sudo modprobe vcan
+fi
+
+# create vcan0 if it doesn't exist
+if ! ip link show vcan0 > /dev/null 2>&1; then
+    sudo ip link add dev vcan0 type vcan
+fi
+
+# bring up vcan0
+sudo ip link set up vcan0
+
+# start playback
+echo "Playing back CAN log '$LOGFILE' on vcan0..."
+cat ~/Downloads/20250817_015645.log | canplayer vcan0=can0
--- websocket/src/main.rs
+++ websocket/src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::SinkExt;
+use socketcan::{EmbeddedFrame, Frame, tokio::CanSocket};
+use std::fmt;
+use std::sync::Arc;
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::broadcast::{Receiver, channel};
+use tokio_tungstenite::tungstenite::Message;
+
+struct Hex<'a>(&'a [u8]);
+
+impl fmt::UpperHex for Hex<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for &b in self.0 {
+            write!(f, "{:02X}", b)?;
+        }
+        Ok(())
+    }
+}
+
+async fn handle_conn(stream: TcpStream, mut msg_recv: Receiver<Arc<str>>) {
+    let Ok(mut ws) = tokio_tungstenite::accept_async(stream).await else {
+        return;
+    };
+
+    loop {
+        let Ok(msg) = msg_recv.recv().await else {
+            continue;
+        };
+
+        if ws.send(Message::text(&*msg)).await.is_err() {
+            return;
+        }
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> std::io::Result<()> {
+    let can_socket = CanSocket::open("vcan0")?;
+    let tcp_listener = TcpListener::bind("localhost:3333").await?;
+
+    let start_time = SystemTime::now();
+    let time = Instant::now();
+
+    let (message_sender, message_recv) = channel::<Arc<str>>(32);
+
+    // Accept task
+    tokio::spawn(async move {
+        loop {
+            let Ok((tcp_stream, _addr)) = tcp_listener.accept().await else {
+                continue;
+            };
+
+            tokio::spawn(handle_conn(tcp_stream, message_recv.resubscribe()));
+        }
+    });
+
+    loop {
+        let Ok(frame) = can_socket.read_frame().await else {
+            continue;
+        };
+
+        let id = frame.raw_id();
+        let data = frame.data();
+
+        let timestamp = start_time + time.elapsed();
+        // UNSAFE: Time goes forward
+        let timestamp = unsafe { timestamp.duration_since(UNIX_EPOCH).unwrap_unchecked() };
+
+        let _ = message_sender.send(Arc::from(format!(
+            "({}.{}) can0 {}#{:X}",
+            timestamp.as_secs(),
+            timestamp.subsec_nanos(),
+            id,
+            Hex(data)
+        )));
+    }
 }
