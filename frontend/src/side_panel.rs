44ce472c32eeb52ade932b1a93c845c090b325c1
Asempere123123 <asempere158@gmail.com>
2025-10-22 16:14:37 +0200

    Added select log file button

--- frontend/src/side_panel.rs
+++ frontend/src/side_panel.rs
@@ -3,11 +3,10 @@ use rfd::AsyncFileDialog;
 use std::{cell::RefCell, rc::Rc, sync::Arc};
 use wasm_bindgen_futures::spawn_local;
 
-use crate::{App, dbc::Signal};
+use crate::{App, dbc::Signal, messages::Messages};
 
 impl App {
     pub fn draw_side_panel(&mut self, ctx: &egui::Context, app_handle: Rc<RefCell<App>>) {
-        let ctx_clone = ctx.clone();
         egui::SidePanel::left("dbc_panel")
             .resizable(true)
             .show(ctx, |ui| {
@@ -16,6 +15,27 @@ impl App {
                     ui.heading("Messages:");
                     if ui.button("Clear").clicked() {
                         self.messages.0.clear();
+                    }
+                    if ui.button("Add from log file").clicked() {
+                        let app_handle = app_handle.clone();
+                        let ctx = ctx.clone();
+                        spawn_local(async move {
+                            if let Some(file) = AsyncFileDialog::new()
+                                .add_filter("log Files", &["log", "LOG"])
+                                .set_directory("/")
+                                .pick_file()
+                                .await
+                            {
+                                let bytes = file.read().await;
+                                let file_contents = String::from_utf8_lossy(&bytes);
+
+                                app_handle
+                                    .borrow_mut()
+                                    .messages
+                                    .extend(&Messages::from_string(file_contents));
+                                ctx.request_repaint();
+                            }
+                        });
                     }
                 });
                 ui.label(format!("{}", self.messages.len()));
@@ -32,7 +52,8 @@ impl App {
                 } else {
                     ui.horizontal(|ui| {
                         ui.label("No dbc loaded");
-                        if ui.button("Seleccionar DBC").clicked() {
+                        let ctx = ctx.clone();
+                        if ui.button("Select DBC").clicked() {
                             spawn_local(async move {
                                 if let Some(file) = AsyncFileDialog::new()
                                     .add_filter("DBC Files", &["dbc", "DBC"])
@@ -43,7 +64,7 @@ impl App {
                                     app_handle
                                         .borrow_mut()
                                         .handle_dbc(file.file_name(), Arc::from(file.read().await));
-                                    ctx_clone.request_repaint();
+                                    ctx.request_repaint();
                                 }
                             });
                         }
